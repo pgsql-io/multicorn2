c14099ef41d3626ed326f88835b912a7bcf4116c
denis lussier <denis@lussier.io>
2022-04-18 14:30:27 -0400

    more little fixes

--- sql/multicorn--2.1.sql
+++ sql/multicorn--2.1.sql
@@ -1,13 +1,13 @@
 -- create wrapper with validator and handler
-CREATE OR REPLACE FUNCTION multicorn2_validator (text[], oid)
+CREATE OR REPLACE FUNCTION multicorn_validator (text[], oid)
 RETURNS bool
 AS 'MODULE_PATHNAME'
 LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION multicorn2_handler ()
+CREATE OR REPLACE FUNCTION multicorn_handler ()
 RETURNS fdw_handler
 AS 'MODULE_PATHNAME'
 LANGUAGE C STRICT;
 
-CREATE FOREIGN DATA WRAPPER multicorn2
-VALIDATOR multicorn2_validator HANDLER multicorn2_handler;
+CREATE FOREIGN DATA WRAPPER multicorn
+VALIDATOR multicorn_validator HANDLER multicorn_handler;
