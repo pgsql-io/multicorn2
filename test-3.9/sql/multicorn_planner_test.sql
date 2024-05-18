d75e4e460b8d6962df3d6d8bc703dac4e4c68e56
Mathieu Fenniak <mathieu@fenniak.net>
2024-05-18 12:34:15 -0600

    Avoid crash on RHS functions by not eval'ing Param during explain

--- test-3.9/sql/multicorn_planner_test.sql
+++ test-3.9/sql/multicorn_planner_test.sql
@@ -6,7 +6,7 @@ CREATE server multicorn_srv foreign data wrapper multicorn options (
 );
 CREATE user mapping FOR current_user server multicorn_srv options (usermapping 'test');
 
--- Test for two thing: first, that when a low total row count, 
+-- Test for two thing: first, that when a low total row count,
 -- a full seq scan is used on a join.
 CREATE foreign table testmulticorn (
     test1 character varying,
@@ -23,14 +23,15 @@ explain select * from testmulticorn m1 left outer join testmulticorn m2 on m1.te
 
 DROP foreign table testmulticorn;
 
--- Second, when a total row count is high 
+-- Second, when a total row count is high
 -- a parameterized path is used on the test1 attribute.
 CREATE foreign table testmulticorn (
     test1 character varying,
     test2 character varying
 ) server multicorn_srv options (
     option1 'option1',
-    test_type 'planner'
+    test_type 'planner',
+    noisy_explain 'true'
 );
 
 explain select * from testmulticorn;
